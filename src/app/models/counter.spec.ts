f34314cb393df207f3a6209dc3d581f658cbdea4
ZFan7 <zfan7@slb.com>
2020-11-30 13:28:58 +0800

    feat: should set account when create an instance

--- src/app/models/counter.spec.ts
+++ src/app/models/counter.spec.ts
@@ -4,4 +4,15 @@ describe('Counter', () => {
   it('should create an instance', () => {
     expect(new Counter()).toBeTruthy();
   });
+
+  it('should set account when create an instance', () => {
+    // given
+    const account = 10;
+
+    // when
+    const counter =  new Counter(account);
+
+    // then
+    expect(counter.account).toBe(account);
+  });
 });
