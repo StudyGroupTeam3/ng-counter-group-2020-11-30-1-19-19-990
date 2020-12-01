253b675745e5bb6de30a6c7672222feb6db2d2f3
ZFan7 <zfan7@slb.com>
2020-12-01 11:05:18 +0800

    feat: change test in component by calling the service

--- src/app/counter-group/counter-group.component.spec.ts
+++ src/app/counter-group/counter-group.component.spec.ts
@@ -25,4 +25,56 @@ describe('CounterGroupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should change count of counters when reset size', () => {
+    // given
+
+
+    // when
+    component.service.setSize(10);
+
+    // then
+    expect(component.service.counters.length).toBe(10);
+  });
+
+  it('should set size when create instance', () => {
+    expect(component.service.size).toBe(5);
+  });
+
+  it('should contain counters when create instance', () => {
+    expect(component.service.counters.length).toBe(component.service.size);
+  });
+
+  it('should return sum of all counters when call sum', () => {
+    // given
+    let expectedSum = 0;
+    component.service.counters.forEach(item => {
+      expectedSum += item.account;
+    });
+
+    // when
+    const sum = component.service.sum();
+
+    // then
+    expect(sum).toBe(expectedSum);
+  });
+
+  it('should all count be reset as 0 and sum change when click reset button', () => {
+    // given
+    let value = 1;
+    component.service.counters.forEach(item => {
+      item.account = value;
+      value++;
+    });
+
+    // when
+    component.service.resetCounters();
+    const sum = component.service.sum();
+
+    // then
+    expect(sum).toBe(0);
+    component.service.counters.forEach(item => {
+      expect(item.account).toBe(0);
+    });
+  });
 });
--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -25,4 +25,37 @@ describe('CounterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should increase account by 1 when call increase', () => {
+    // given
+    component.counter.account = 0;
+
+    // when
+    component.counter.increase();
+
+    // then
+    expect(component.counter.account).toBe(1);
+  });
+
+  it('should decrease account by 1 when call decrease', () => {
+    // given
+    component.counter.account = 0;
+
+    // when
+    component.counter.decrease();
+
+    // then
+    expect(component.counter.account).toBe(-1);
+  });
+
+  it('should count be reset as 0 when click reset button', () => {
+    // given
+    component.counter.account = 5;
+
+    // when
+    component.reset();
+
+    // then
+    expect(component.account).toBe(0);
+  });
 });
